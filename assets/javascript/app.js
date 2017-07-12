bf29e39d151f3671d53acbce21e74fe0ff1096bf
Blake Desktop <bhhuynh@ucsd.edu>
2017-07-12 00:37:06 -0700

    font fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-//set date
+//set date to countdown
 var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
 
 // Update the count down every 1 second
@@ -15,6 +15,17 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //Add leading 0 if < 10 for formatting
+  if(hours<10){
+    hours = "0"+hours;
+  }
+  if(minutes<10){
+    minutes = "0"+minutes;
+  }
+  if(seconds<10){
+    seconds = "0"+seconds;
+  }
 
   // Display the result in the element with id="demo"
   $(".days").html(days+" Days");
