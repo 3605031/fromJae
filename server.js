941cd4d0cc3f6b29f7ce4496f08b0894fa8f7ba0
Nate <bhhuynh@ucsd.edu>
2017-07-18 22:05:35 -0700

    feature item 2 fix

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 var mongodb = require('mongodb');
+var exphbs = require("express-handlebars");
 
 var MongoClient = mongodb.MongoClient;
+
+
+
 
 
 
@@ -13,8 +17,9 @@ var MongoClient = mongodb.MongoClient;
 var app = express();
 var port = process.env.PORT || 3000;
 
-// === Connecting mongoose
-// mongoose.connect("localhost:27017/fromjae");
+//Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine","handlebars")
 
 
 // Sets up the Express app to handle data parsing
@@ -41,9 +46,9 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "homepage.html"));
 });
 
-var products;
+
 app.get("/api/:query?", function(req, res) {
-    let chosen = req.params.query;
+    var chosen = req.params.query;
     //Temporary solution without knowing React + Handlebars
     var url = 'mongodb://Blake:Soithan1995@ds034677.mlab.com:34677/fromjae';
     switch(chosen){
@@ -77,4 +82,39 @@ app.get("/api/:query?", function(req, res) {
 
 
 
-                            
+// Data
+var lunches = [
+  {
+    lunch: "Beet & Goat Cheese Salad with minestrone soup."
+  }, {
+    lunch: "Pizza, two double veggie burgers, fries with a big glup"
+  }
+];
+// Routes
+app.get("/weekday", function(req, res) {
+    var url = 'mongodb://Blake:Soithan1995@ds034677.mlab.com:34677/fromjae';
+    MongoClient.connect(url, function(err, db) {
+            if(err){
+                console.log("can't connect",err)
+            } else {
+                //Connected
+                console.log("connection established");
+                //Get inventory collection
+                var collection = db.collection('inventory');
+                //Find all inside inventory
+                collection.find({}).toArray(function(error,result){
+                    if(error){
+                        res.send(error);
+                    } else if (result.length){
+                        console.log(result[0].url);
+                        res.render("index",{lunch:result[0].url});
+                    } else {
+                        res.send('nothing is found');
+                    }
+                });
+                //Close connection
+                db.close();
+            }
+        })
+});
+              
